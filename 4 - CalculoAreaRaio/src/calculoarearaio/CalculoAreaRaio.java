f6f0b84009cd263beeeeeea2f50e25025607e5a1
viniciustvf <vini.tvf@hotmail.com>
2022-09-23 21:26:48 -0300

    new exercise

---
+++ 4 - CalculoAreaRaio/src/calculoarearaio/CalculoAreaRaio.java
@@ -0,0 +1,20 @@
+package calculoarearaio;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CalculoAreaRaio {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        
+        Scanner ent = new Scanner(System.in);
+        System.out.println("DIGITE O RAIO: ");
+        
+        double raio = ent.nextDouble();
+        double raio2 = Math.pow(raio, 2.0);
+        double area = Math.PI * raio2;
+        
+        System.out.printf("AREA: %.4f ", area);
+    }
+}
