c457619f8bdd94f4a21ffbd003f86798258b6dc1
vinicius7vf <vini.tvf@hotmail.com>
2023-03-30 23:38:57 -0300

    New exercise

---
+++ 16 - Tabuleiro/src/entities/Jogo.java
@@ -0,0 +1,46 @@
+
+package entities;
+
+
+public class Jogo {
+    Tabuleiro meuTabuleiro;
+    Jogador[] meusJogadores;
+    Dado meuDado;
+
+    public Jogo() {
+    }
+
+    public Jogo(Jogador[] meusJogadores, Dado meuDado) {
+        this.meusJogadores = meusJogadores;
+        this.meuDado = meuDado;
+    }
+
+    public Tabuleiro getMeuTabuleiro() {
+        return meuTabuleiro;
+    }
+
+    public void setMeuTabuleiro(Tabuleiro meuTabuleiro) {
+        this.meuTabuleiro = meuTabuleiro;
+    }
+
+    public Jogador[] getMeusJogadores() {
+        return meusJogadores;
+    }
+
+    public void setMeusJogadores(Jogador[] meusJogadores) {
+        this.meusJogadores = meusJogadores;
+    }
+
+    public Dado getMeuDado() {
+        return meuDado;
+    }
+
+    public void setMeuDado(Dado meuDado) {
+        this.meuDado = meuDado;
+    }
+    
+    
+    public void proximaJogada(){
+        
+    }
+}
---
+++ 16 - Tabuleiro/src/entities/Tabuleiro.java
@@ -0,0 +1,19 @@
+
+package entities;
+
+
+public class Tabuleiro {
+    Integer numeroCasas = 20;
+
+    public Tabuleiro() {
+    }
+
+    public Integer getNumeroCasas() {
+        return numeroCasas;
+    }
+
+    public void setNumeroCasas(Integer numeroCasas) {
+        this.numeroCasas = numeroCasas;
+    }
+    
+}
